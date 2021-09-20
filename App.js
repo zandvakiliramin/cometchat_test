dc100b34ca22b950b7fd6debf7e682c51c73ee9b
Ramin Zandvakili <zandv003@umn.edu>
2021-09-19 19:09:26 -0500

    initial commit working

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import * as CONSTANTS from "./constants/constant";
+import { CometChat } from "@cometchat-pro/react-native-chat";
+
+const App = () => {
+  var appSetting = new CometChat.AppSettingsBuilder()
+    .subscribePresenceForAllUsers()
+    .setRegion(CONSTANTS.APP_REGION)
+    .build();
+
+  CometChat.init(CONSTANTS.APP_ID, appSetting).then(
+    () => {
+      console.log("Initialization completed successfully");
+      var UID = "SUPERHERO1";
+      var authKey = CONSTANTS.AUTH_KEY;
+
+      // You can now call login function.
+      CometChat.login(UID, authKey).then(
+        (user) => {
+          console.log("Login Successful:", { user });
+        },
+        (error) => {
+          console.log("Login failed with exception:", { error });
+        }
+      );
+    },
+    (error) => {
+      console.log("Initialization failed with error:", error);
+      // Check the reason for error and take appropriate action.
+    }
+  );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
---
+++ constants/constant.js
@@ -0,0 +1,3 @@
+export const APP_ID = "194020a1507abb67";
+export const APP_REGION = "us";
+export const AUTH_KEY = "7dff62f6c4b64dc15bfaa69bd5d1651cf5f1ba26";
